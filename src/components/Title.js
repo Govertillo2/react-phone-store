2636026378114139cac80f0fae9ef9c2bff5dcc5
Ken Kole <kennyklose@gmil.com>
2020-04-09 10:01:31 +0200

    Hola

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Title({name, title}) {
+    return (
+        <div className="row">
+        <div className="text-title">
+      <h1 className="text-capitalize font-weight-bold">
+          {name} <br></br><strong className="text-blue">{title}</strong>
+      </h1>
+      
+      </div>      
+        </div>
+    )
+}
+
